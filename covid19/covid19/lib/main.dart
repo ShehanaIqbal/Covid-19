20e8f6c3252a07f9bf463db3777d8bde0271efc2
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2020-04-16 19:58:59 +0530

    create signup page and authentication

--- covid19/covid19/lib/main.dart
+++ covid19/covid19/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:covid19/screens/homepage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:covid19/screens/homepage.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import './screens/signup.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WelcomeScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: WelcomeScreen(),
+        home: Signup());
   }
 }
