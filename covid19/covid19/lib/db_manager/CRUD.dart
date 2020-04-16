946f57e8e47a18079e55ab5ca5271ea2f250bfa3
Kalpa D <KALPAFERNANDO1998@gmail.com>
2020-04-16 17:51:38 +0530

    CRUDManager uncommented

---
+++ covid19/covid19/lib/db_manager/CRUD.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CRUDManager {
+  final db = Firestore.instance;
+
+  createData(String dbname, dynamic json) async {
+    DocumentReference ref = await db.collection(dbname).add(json);
+    return ref.documentID;
+  }
+
+  void readData(String dbname, String id) async {
+    DocumentSnapshot snapshot = await db.collection(dbname).document(id).get();
+  }
+
+  void updateData(DocumentSnapshot doc, String dbname, dynamic json) async {
+    await db.collection(dbname).document(doc.documentID).updateData(json);
+  }
+
+  void deleteData(DocumentSnapshot doc, String dbname) async {
+    await db.collection(dbname).document(doc.documentID).delete();
+  }
+}
