5633facaedbe1576750faddbd2aef7817a91f080
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2020-04-16 19:32:30 +0530

    make clear

--- covid19/covid19/lib/db_manager/CRUD.dart
+++ covid19/covid19/lib/db_manager/CRUD.dart
@@ -4,19 +4,23 @@ class CRUDManager {
   final db = Firestore.instance;
 
   createData(String dbname, dynamic json) async {
+    // add users and add bluetooth addresses
     DocumentReference ref = await db.collection(dbname).add(json);
     return ref.documentID;
   }
 
   void readData(String dbname, String id) async {
+    //use to read user data and get the met people
     DocumentSnapshot snapshot = await db.collection(dbname).document(id).get();
   }
 
   void updateData(DocumentSnapshot doc, String dbname, dynamic json) async {
+    //use to update the uder mobile number
     await db.collection(dbname).document(doc.documentID).updateData(json);
   }
 
   void deleteData(DocumentSnapshot doc, String dbname) async {
+    // use to delete the userdata after 14 days
     await db.collection(dbname).document(doc.documentID).delete();
   }
 }
